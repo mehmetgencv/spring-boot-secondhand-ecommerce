d2c78290aa854e5c53a5b235f6a0f3efd8fa8964
mehmetgencv <mehmetgencv@gmail.com>
2023-12-23 17:24:48 +0300

    update starters

--- src/main/java/com/mehmetgenc/secondhand/SecondhandApplication.java
+++ src/main/java/com/mehmetgenc/secondhand/SecondhandApplication.java
@@ -2,8 +2,10 @@ package com.mehmetgenc.secondhand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.mehmetgenc.secondhand.user"})
 public class SecondhandApplication {
 
 	public static void main(String[] args) {
---
+++ user/src/main/java/com/mehmetgenc/secondhand/user/UserApplication.java
@@ -0,0 +1,10 @@
+package com.mehmetgenc.secondhand.user;
+
+//@SpringBootApplication
+public class UserApplication {
+
+/*    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }*/
+
+}
