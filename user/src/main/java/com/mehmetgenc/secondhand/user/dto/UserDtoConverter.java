2d5e1f7cb50cf3cf3943ae61fe09ca0ebe28d7f2
mehmetgencv <mehmetgencv@gmail.com>
2023-12-25 21:07:14 +0300

    add UserDetails

--- user/src/main/java/com/mehmetgenc/secondhand/user/dto/UserDtoConverter.java
+++ user/src/main/java/com/mehmetgenc/secondhand/user/dto/UserDtoConverter.java
@@ -1,29 +1,32 @@
 package com.mehmetgenc.secondhand.user.dto;
 
 import com.mehmetgenc.secondhand.user.model.User;
-import com.mehmetgenc.secondhand.user.model.User;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class UserDtoConverter {
+    private final UserDetailsDtoConverter converter;
+
+    public UserDtoConverter(UserDetailsDtoConverter converter) {
+        this.converter = converter;
+    }
+
     public UserDto convert(User from){
         return new UserDto(
                 from.getMail(),
                 from.getFirstName(),
                 from.getMiddleName(),
-                from.getLastName());
+                from.getLastName(),
+                converter.convert(new ArrayList<>(from.getUserDetailsSet())));
     }
 
     public List<UserDto> convert(List<User> fromList){
         return fromList.stream()
-                .map(from -> new UserDto(
-                        from.getMail(),
-                        from.getFirstName(),
-                        from.getMiddleName(),
-                        from.getLastName()))
+                .map(this::convert)
                 .collect(Collectors.toList());
     }
 }
