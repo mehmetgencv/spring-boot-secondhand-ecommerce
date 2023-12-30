4bbe575957b42eae0396487c5cbe6d238c4bbaa5
mehmetgencv <mehmetgencv@gmail.com>
2023-12-30 23:38:44 +0300

    update Advertisement

--- advertisement/src/main/java/org/mehmetgenc/secondhand/advertisement/model/Advertisement.java
+++ advertisement/src/main/java/org/mehmetgenc/secondhand/advertisement/model/Advertisement.java
@@ -1,17 +1,28 @@
 package org.mehmetgenc.secondhand.advertisement.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
+@Entity(name = "advertisement")
 @Document(indexName = "advertisement")
 public class Advertisement {
 
     @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     private String id;
 
     @Field(type = FieldType.Keyword)
@@ -22,25 +33,53 @@ public class Advertisement {
 
     private Double price;
     @Field(type = FieldType.Date, format = DateFormat.basic_date_time)
-    private Date creationDate;
+    private LocalDateTime creationDate;
     @Field(type = FieldType.Date, format = DateFormat.basic_date_time)
-    private Date lastModifiedDate;
+    private LocalDateTime lastModifiedDate;
+
+    private Long userId;
+
+    private Set<String> hashtags = new HashSet<>();
 
     public Advertisement() {
     }
 
-    public Advertisement(String title, String description, Double price, Date creationDate, Date lastModifiedDate) {
+    public Advertisement(String title, String description, Double price, LocalDateTime creationDate, LocalDateTime lastModifiedDate, Long userId, Set<String> hashtags) {
         this.title = title;
         this.description = description;
         this.price = price;
         this.creationDate = creationDate;
         this.lastModifiedDate = lastModifiedDate;
+        this.userId = userId;
+        this.hashtags = hashtags;
     }
 
-    public Advertisement(String title, String description, Double price) {
+    public Advertisement(String title, String description, Double price, Long userId, Set<String> hashtags) {
         this.title = title;
         this.description = description;
         this.price = price;
+        this.creationDate = LocalDateTime.now();
+        this.lastModifiedDate = LocalDateTime.now();
+        this.userId = userId;
+        this.hashtags = hashtags;
+    }
+
+    public Advertisement(String id,
+                         String title,
+                         String description,
+                         Double price,
+                         LocalDateTime creationDate,
+                         LocalDateTime lastModifiedDate,
+                         Long userId,
+                         Set<String> hashtags) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.creationDate = creationDate;
+        this.lastModifiedDate = lastModifiedDate;
+        this.userId = userId;
+        this.hashtags = hashtags;
     }
 
     public String getId() {
@@ -59,12 +98,20 @@ public class Advertisement {
         return price;
     }
 
-    public Date getCreationDate() {
+    public LocalDateTime getCreationDate() {
         return creationDate;
     }
 
-    public Date getLastModifiedDate() {
+    public LocalDateTime getLastModifiedDate() {
         return lastModifiedDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public Set<String> getHashtags() {
+        return hashtags;
     }
 
     @Override
@@ -76,6 +123,21 @@ public class Advertisement {
                 ", price=" + price +
                 ", creationDate=" + creationDate +
                 ", lastModifiedDate=" + lastModifiedDate +
+                ", userId=" + userId +
+                ", hashtags=" + hashtags +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Advertisement that = (Advertisement) o;
+        return Objects.equals(id, that.id) && Objects.equals(title, that.title) && Objects.equals(description, that.description) && Objects.equals(price, that.price) && Objects.equals(creationDate, that.creationDate) && Objects.equals(lastModifiedDate, that.lastModifiedDate) && Objects.equals(userId, that.userId) && Objects.equals(hashtags, that.hashtags);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, price, creationDate, lastModifiedDate, userId, hashtags);
     }
 }
